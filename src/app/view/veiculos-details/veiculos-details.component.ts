a2e40121ca096766229f2acef57438449556a11b
Plínio Pvv <gangss2@hotmail.com>
2024-08-31 11:32:28 -0300

    +details

--- src/app/view/veiculos-details/veiculos-details.component.ts
+++ src/app/view/veiculos-details/veiculos-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { Veiculo } from '../../model/veiculo';
+import { VeiculosService } from '../../service/veiculos.service';
 
 @Component({
   selector: 'app-veiculos-details',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './veiculos-details.component.scss'
 })
 export class VeiculosDetailsComponent {
+
+  model: Veiculo;
+
+  constructor(private route: ActivatedRoute, private service: VeiculosService) {
+    const id: Observable<string> = route.params.pipe(map((p) => p['id']));
+
+    id.subscribe(async id => {
+      this.model = await this.service.get(parseInt(id));
+    });
+  }
+
+  alugar() {
+    alert("Veículo alugado com sukissess");
+  }
 
 }
