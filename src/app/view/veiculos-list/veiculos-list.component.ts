235c676b6b83fd9e48030dafe51819ec5a561f29
Plínio Pvv <gangss2@hotmail.com>
2024-08-31 11:16:32 -0300

    +view

--- src/app/view/veiculos-list/veiculos-list.component.ts
+++ src/app/view/veiculos-list/veiculos-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VeiculosService } from '../../service/veiculos.service';
+import { Veiculo } from '../../model/veiculo';
 
 @Component({
   selector: 'app-veiculos-list',
   templateUrl: './veiculos-list.component.html',
   styleUrl: './veiculos-list.component.scss'
 })
-export class VeiculosListComponent {
+export class VeiculosListComponent implements OnInit {
+
+  veiculos: Veiculo[] = [];
+
+  constructor(private service: VeiculosService) {}
+
+  async ngOnInit(): Promise<void> {
+    const list = await this.service.list();
+    this.veiculos = list;
+  }
 
 }
